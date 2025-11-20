5a15fb6ffe525c673b9fe2f3bc2b0ca721c7c86a
fireman-dev <shayanofduty@gmail.com>
2025-11-20 22:38:59 +0330

    Update App.xaml.cs

--- src/FireNet.UI/App.xaml.cs
+++ src/FireNet.UI/App.xaml.cs
@@ -1,8 +1,51 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace FireNet.UI
 {
     public partial class App : Application
     {
+        public App()
+        {
+            try
+            {
+                Directory.CreateDirectory("logs");
+            }
+            catch { }
+
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                try
+                {
+                    File.AppendAllText("logs/crash.log",
+                        $"[UnhandledException]\n{DateTime.Now}\n{e.ExceptionObject}\n\n");
+                }
+                catch { }
+            };
+
+            DispatcherUnhandledException += (s, e) =>
+            {
+                try
+                {
+                    File.AppendAllText("logs/ui-crash.log",
+                        $"[UI Exception]\n{DateTime.Now}\n{e.Exception}\n\n");
+                }
+                catch { }
+
+                e.Handled = true;
+            };
+
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                try
+                {
+                    File.AppendAllText("logs/task-crash.log",
+                        $"[Task Exception]\n{DateTime.Now}\n{e.Exception}\n\n");
+                }
+                catch { }
+            };
+        }
     }
 }
